e51d766df50b92856a1f2da2ef354509715f2183
Evan Bartels <ebartels1@ewu.edu>
2023-05-03 12:33:37 -0700

    Made adjustments to event creation sites to allow for drop down coming from sql event_categories table

--- createEvent.php
+++ createEvent.php
@@ -1,71 +1,65 @@
 <?php
-
-require_once "config.php";
-
-// Initialize the session
-session_start();
- 
-// Check if the user is logged in, if not then redirect him to login page
-if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
-    header("location: login.php");
-    exit;
-}
- 
-if($_SERVER["REQUEST_METHOD"] == "POST"){
-
-    //print_r($_POST);
-
-    $title = $_POST['title'];
-    $description = $_POST['description'];
-    $date = date('Y-m-d', strtotime($_POST['date']));
-
-    $start_time = ($_POST['startTime']);
-    $end_time = ($_POST['endTime']);
-
-    //$start_time = $_POST['startTime'];
-    //$end_time = $_POST['endTime'];
-
-    $location = $_POST['location'];
-    $capacity = $_POST['capacity'];
-    $organizer_id = $_SESSION['UID'];
-
-    $category = 1;
-
-    $sql = "INSERT INTO events (organizer_id, title, description, date, start_time, end_time, location, capacity, category) 
-            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
-
-    $stmt = mysqli_stmt_init($conn);
-
-    if ( ! mysqli_stmt_prepare($stmt, $sql)) {
-        die(mysqli_error($conn));
-    }
-
-    mysqli_stmt_bind_param($stmt, "issssssii", $organizer_id, $title, $description, $date, $start_time, $end_time, $location, $capacity, $category);
-
-    mysqli_stmt_execute($stmt);
-
-    echo "Event added";
-    header("Location: Dashboard.html");
-
-
-
-    /*
-    $sql = "INSERT INTO events (organizer_id, title, description, date, start_time, end_time, location, capacity) 
-            VALUES ($organizer_id, '$title', '$description', $date, $start_time, $end_time, '$location', $capacity)";
-    
-    $query_run = mysqli_query($conn, $sql);
-
-    if($query_run)
-    {
-        $_SESSION['status'] = "Date values Inserted";
-        header("Location: Dashboard.html");
-    }
-    else
-    {
-        $_SESSION['status'] = "Date values Inserting Failed";
-        header("Location: Dashboard.html");
-    }
-    */
-}
-
+    require_once "config.php";
+    $resultSet = $conn->query("SELECT name FROM event_categories")
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <title>Create Event</title>
+   <link rel="stylesheet" href="createEvent.css">
+</head>
+<body>
+   <h1>Create Event</h1>
+   
+   <!-- Add the form below -->
+    <form method="POST" action="eventCreation.php" enctype="multipart/form-data">
+        <div>
+            <label for="title">Title:</label>
+            <input type="text" name="title" id="title" required>
+        </div>
+        <div>
+            <label for="description">Description:</label>
+            <textarea name="description" id="description" rows="3" cols="50"></textarea>
+        </div>
+        <div>
+            <label for="date">Date:</label>
+            <input type="date" name="date" id="date" required>
+        </div>
+        <div>
+            <label for="startTime">Start Time:</label>
+            <input type="time" name="startTime" id="startTime" required>
+        </div>
+        <div>
+            <label for="endTime">End Time:</label>
+            <input type="time" name="endTime" id="endTime" required>
+        </div>
+        <div>
+            <label for="location">Location:</label>
+            <input type="text" name="location" id="location" required>
+        </div>
+        <div>
+            <label for="capacity">Capacity:</label>
+            <input type="number" name="capacity" id="capacity" required>
+        </div>
+        <div>
+            <label for="category">Category:</label>
+            <select name="category" id="category" required>
+                <?php
+                while($rows = $resultSet->fetch_assoc()){
+                    $category = $rows['name'];
+                    echo "<option value='$category'>$category</option>";
+                }
+                ?>
+        </div>
+        <div><input type="submit"></div>
+    </form>
+   
+</body>
+    <!--// First create a SQL Query and obtain the row 
+    // select dropdown_items from table
+    // Send this as an array json to JavaScript
+    // Access it with jQuery & Ajax in Javascript, and then in a for loop print it out as HTML 
+    // https://www.w3schools.com/html/html_form_elements.asp-->
+
+</html>
---
+++ eventCreation.php
@@ -0,0 +1,76 @@
+<?php
+
+require_once "config.php";
+
+// Initialize the session
+session_start();
+ 
+// Check if the user is logged in, if not then redirect him to login page
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: login.php");
+    exit;
+}
+ 
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+    //print_r($_POST);
+
+    $title = $_POST['title'];
+    $description = $_POST['description'];
+    $date = date('Y-m-d', strtotime($_POST['date']));
+
+    $start_time = ($_POST['startTime']);
+    $end_time = ($_POST['endTime']);
+
+    //$start_time = $_POST['startTime'];
+    //$end_time = $_POST['endTime'];
+
+    $location = $_POST['location'];
+    $capacity = $_POST['capacity'];
+    $organizer_id = $_SESSION['UID'];
+
+    $category = $_POST['category'];
+    $QueryForCat_id = $conn->query("SELECT Cat_id FROM event_categories WHERE name='$category'");
+    $cat_id = 0;
+    while ($row = $QueryForCat_id->fetch_assoc()){
+        $cat_id = $row["Cat_id"];
+    }
+
+    $sql = "INSERT INTO events (organizer_id, title, description, date, start_time, end_time, location, capacity, category) 
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+    $stmt = mysqli_stmt_init($conn);
+
+    if ( ! mysqli_stmt_prepare($stmt, $sql)) {
+        die(mysqli_error($conn));
+    }
+
+    mysqli_stmt_bind_param($stmt, "issssssii", $organizer_id, $title, $description, $date, $start_time, $end_time, $location, $capacity, $cat_id);
+
+    mysqli_stmt_execute($stmt);
+
+    echo "Event added";
+    header("Location: Dashboard.html");
+
+
+
+    /*
+    $sql = "INSERT INTO events (organizer_id, title, description, date, start_time, end_time, location, capacity) 
+            VALUES ($organizer_id, '$title', '$description', $date, $start_time, $end_time, '$location', $capacity)";
+    
+    $query_run = mysqli_query($conn, $sql);
+
+    if($query_run)
+    {
+        $_SESSION['status'] = "Date values Inserted";
+        header("Location: Dashboard.html");
+    }
+    else
+    {
+        $_SESSION['status'] = "Date values Inserting Failed";
+        header("Location: Dashboard.html");
+    }
+    */
+}
+
+?>
