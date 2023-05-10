a27fe8beac7cabe69c563b4ef9d054a6ce2517b9
Evan Bartels <ebartels1@ewu.edu>
2023-05-10 11:11:14 -0700

    small fix

--- createEvent.php
+++ createEvent.php
@@ -1,6 +1,15 @@
 <?php
     require_once "config.php";
-    $resultSet = $conn->query("SELECT name FROM event_categories")
+    $resultSet = $conn->query("SELECT name FROM event_categories");
+
+    // Initialize the session
+    session_start();
+    
+    // Check if the user is logged in, if not then redirect him to login page
+    if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+        header("location: login.php");
+        exit;
+}
 ?>
 
 <!DOCTYPE html>
