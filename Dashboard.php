f98cb4ce2cd7002c8b92974cb5b7459f9c0a6baf
Nick Parkman <nparkman@ewu.edu>
2023-05-10 09:38:32 -0700

    Quick Dashboard.php

---
+++ Dashboard.php
@@ -0,0 +1,52 @@
+<?php
+// Initialize the session
+session_start();
+ 
+// Check if the user is logged in, if not then redirect him to login page
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: login.php");
+    exit;
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en"> 
+    <head>
+        <title>Dashboard</title>
+        <link rel="stylesheet" href="Dashboard.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    </head>
+    <body>
+        <div class="topNav">
+            <div class="active">Dashboard</div>
+            <div id="myLinks">
+                <a href='search.html'>
+                    Search Events
+                </a>
+                <a href='YourEvents.html'>
+                    Your Events
+                </a>
+                <a href='createEvent.php'>
+                    Create Event
+                </a>
+                <a href="logout.php">
+                    Sign Out 
+                </a>
+            </div>
+            <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+                <i class="fa fa-bars"></i>
+            </a>
+        </div>
+        <h1>Welcome <b><?php echo htmlspecialchars($_SESSION["first_name"]); ?></b>!</h1>
+        <script>
+            function myFunction() {
+                var x = document.getElementById("myLinks");
+                if (x.style.display === "block") {
+                    x.style.display = "none";
+                } else {
+                    x.style.display = "block";
+                }
+            }
+        </script>
+    </body>
+</html>
