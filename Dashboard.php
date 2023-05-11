490f408b55c15d4c94407244592656d2cc8c652f
Evan Bartels <ebartels1@ewu.edu>
2023-05-11 12:59:57 -0700

    Made it so you can see your events in dashboard, this will work in your event tab but not what we want for the dashboard

--- Dashboard.php
+++ Dashboard.php
@@ -7,6 +7,12 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     header("location: login.php");
     exit;
 }
+
+require_once "config.php";
+$UID = $_SESSION["UID"];
+$query = "SELECT title, description, date, start_time, end_time, location FROM events WHERE organizer_id='$UID' ORDER BY date";
+$result = mysqli_query($conn, $query);
+
 ?>
 
 <!DOCTYPE html>
@@ -38,6 +44,59 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
             </a>
         </div>
         <h1>Welcome <b><?php echo htmlspecialchars($_SESSION["first_name"]); ?></b>!</h1>
+        
+        <div class = "container">
+            <div class = "row">
+                <div class = "col">
+                    <div class = "card">
+                        <div class = "card-header">
+                            <h2>Events you are participating in: </h2>
+                        </div>
+                        <div class = "card-body">
+                            <table class = "table", border = "4", style = "border-color: darkblue", width = 100%>
+                                <tr class = "table-header", style = "background-color: turquoise">
+                                    <td> Events </td>
+                                    <td> Description </td>
+                                    <td> Date </td>
+                                    <td> Start Time </td>
+                                    <td> End Time </td>
+                                    <td> Location </td>
+                                    <td> Leave Event? </td>
+                                </tr>
+                                <tr>
+                                <?php
+                                    if (mysqli_num_rows($result)>0){
+                                        while($row = mysqli_fetch_assoc($result)) {
+                                ?>
+                                    <?php   //idk if i need to get rid of this php here.
+                                            //echo "Event: " . $row["title"] . " - Event Description: " . $row["description"] . 
+                                            //" - Date: " . $row["date"] . " - Start time: " . $row["start_time"] . " - End Time: " . 
+                                            //$row["end_time"] . " - Location: " . $row["location"]. "<br>";
+                                    ?>
+                                    <td><?php echo $row["title"]; ?></td>
+                                    <td><?php echo $row["description"]; ?></td>
+                                    <td><?php echo $row["date"]; ?></td>
+                                    <td><?php echo $row["start_time"]; ?></td>
+                                    <td><?php echo $row["end_time"]; ?></td>
+                                    <td><?php echo $row["location"]; ?></td>
+                                    <td><a href = "#" class="btn btn-danger">Leave</a></td>
+                                </tr>    
+                                <?php    
+                                        }
+                                    }
+                                    else {
+                                        echo "No events scheduled. Go find some!";
+                                    }
+
+                                    $conn->close(); 
+                                ?>
+                                </tr>
+                            </table>     
+                        </div>
+                    </div>
+                </div>
+            </div>            
+        </div>    
         <script>
             function myFunction() {
                 var x = document.getElementById("myLinks");
