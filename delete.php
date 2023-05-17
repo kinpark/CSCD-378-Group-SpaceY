9a699c4c397a9f33b6e0ab8da5a4c9da2a797c94
Evan Bartels <ebartels1@ewu.edu>
2023-05-17 11:23:10 -0700

    Fixed delete so owners can remove event if somebody has signed up for it

--- delete.php
+++ delete.php
@@ -1,6 +1,10 @@
 <?php
     require_once "config.php";
     $EID = $_GET["EID"];
+
+    $query = "DELETE FROM registration WHERE event_id='$EID'";
+    mysqli_query($conn, $query);
+
     $query = "DELETE FROM events WHERE EID='$EID'";
 	mysqli_query($conn, $query);
 	header('location:YourEvents.php');
